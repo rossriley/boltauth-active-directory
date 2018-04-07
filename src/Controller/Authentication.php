b1509c5d8ac4b4043792d439bf88da9ea96f6e44
Ross Riley <riley.ross@gmail.com>
2018-04-07 23:37:49 +0100

    fix the lookup of handler

--- src/Controller/Authentication.php
+++ src/Controller/Authentication.php
@@ -63,7 +63,7 @@ class Authentication extends BoltAuthAuthentication
         $this->getAuthOauthProviderManager()->setProvider($app, 'activedirectory');
 
         /** @var ActiveDirectory $handler */
-        $handler = $this->getContainer()['auth.oauth.provider.activedirectory']();
+        $handler = $this->getAuthOauthHandler();
         $handler->setSubmittedForm($passwordForm);
 
         // Initial login checks
