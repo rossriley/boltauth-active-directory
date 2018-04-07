ea97ff9f25b88fa45d75f804a64c57d4599bbe11
Ross Riley <riley.ross@gmail.com>
2018-04-07 23:16:04 +0100

    process the config to make a connection to the LDAP server

--- src/Handler/ActiveDirectory.php
+++ src/Handler/ActiveDirectory.php
@@ -3,31 +3,69 @@
 namespace Bolt\Extension\BoltAuth\ActiveDirectory\Handler;
 
 
-use Bolt\Extension\BoltAuth\Auth\Exception\DisabledProviderException;
-use Bolt\Extension\BoltAuth\Auth\Exception\InvalidAuthorisationRequestException;
 use Bolt\Extension\BoltAuth\Auth\Oauth2\Handler\HandlerInterface;
+use Bolt\Extension\BoltAuth\Auth\Oauth2\Handler\Local;
+use LDAP;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 
-class ActiveDirectory implements HandlerInterface
+class ActiveDirectory extends Local implements HandlerInterface
 {
-    protected $config;
-    protected $container;
 
-    public function __construct($config, $container)
-    {
-        $this->config = $config;
-        $this->container = $container;
-    }
+    /** @var Form */
+    private $submittedForm;
 
     /**
      * Login a client.
      *
      * @param Request $request
-     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \InvalidArgumentException
+     * @throws RuntimeException
+     * @throws \Bolt\Extension\BoltAuth\Auth\Exception\DisabledProviderException
+     * @throws \Exception
      */
     public function login(Request $request)
     {
-        // TODO: Implement login() method.
+        if (parent::login($request)) {
+            return $this->session->popRedirect()->getResponse();
+        }
+
+        if ($this->submittedForm === null) {
+            throw new RuntimeException(sprintf('%s requires a %s object to be set.', __METHOD__, Form::class));
+        }
+
+        $adConfig = $this->config->getProvider('activedirectory');
+        $serverConfig = $adConfig->getOptions()['server'];
+
+        if (!isset($serverConfig['domain'])) {
+            throw new RuntimeException(sprintf('%s via Active Directory requires a %s configuration to be set.', __METHOD__, 'server:domain'));
+        }
+
+        if (!isset($serverConfig['org'])) {
+            throw new RuntimeException(sprintf('%s via Active Directory requires a %s configuration to be set.', __METHOD__, 'server:org'));
+        }
+        if (!isset($serverConfig['user'])) {
+            throw new RuntimeException(sprintf('%s via Active Directory requires a %s configuration to be set.', __METHOD__, 'server:user'));
+        }
+
+        $domain = $serverConfig['domain'];
+        $port = $serverConfig['port'] ?: null;
+        $ou = $serverConfig['ou'] ?: 'Users';
+        $org = $serverConfig['org'];
+        $adminUser = $serverConfig['user'];
+        $adminPass = $serverConfig['password'];
+        $dcs = $serverConfig['dc'];
+        $bindDN = sprintf('uid=%s,ou=%s,o=%s,dc=%s', $adminUser, $ou, $org, implode(',', $dcs));
+
+
+        $email = $this->submittedForm->get('email')->getData();
+        $username = strstr($email, '@', true);
+        $password = $this->submittedForm->get('password')->getData();
+        $ldap = new LDAP($domain, $port);
+        $authUser = $ldap->checkLogin($username, $password, null, null, $domain, $bindDN, $adminPass);
+
+        dump($authUser); exit;
     }
 
     /**
@@ -36,7 +74,7 @@ class ActiveDirectory implements HandlerInterface
      * @param Request $request
      * @param string $grantType
      */
-    public function process(Request $request, $grantType)
+    public function process(Request $request, $grantType = 'password')
     {
         // TODO: Implement process() method.
     }
